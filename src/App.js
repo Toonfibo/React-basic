60655d9a8c3a1463787352abe02258aea84be4e4
Toonfibo <37840209+Toonfibo@users.noreply.github.com>
2022-08-24 18:27:54 +0700

    final React-basic

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
+import React , {useState, useEffect} from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.css';        
+import'./MyCard.css';                      
+import Navbar from './Navbar';
+import MyCard from './MyCard';
+
 
 function App() {
+  const [data, setData] = useState([])
+  useEffect(()=> {
+    fetch("https://www.mecallapi.com/api/attractions")
+    .then(res => res.json())
+    .then((result) => {setData(result)})
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+     <Navbar />
+     <h1 style={{paddingLeft: '10px'}}> Attractions</h1>
+     <div className='grid-container'>
+      {data.map(item => (
+         <MyCard key={item.id} name={item.name} coverimage={item.coverimage} detail={item.detail} />
+      ))}
+       
+        
     </div>
+    </>
+    
+    
   );
 }
 
